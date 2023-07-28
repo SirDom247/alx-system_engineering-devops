88f712ab6705e2c88ed5791ac319e155908dc11a
Dominic <dominicudousoro@gmail.com>
2023-07-27 23:04:10 -0700

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to JSON format."""
-#import json
-#import requests
-#import sys
+import json
+import requests
+import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
